f8fbfc47fc6aa9a4fde12529c12fd0ca7adf3a13
Paweł Fidelus <fidelusp@gmail.com>
2021-09-01 17:33:01 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, SyntheticEvent, useEffect } from 'react'
+import './App.css'
 
-function App() {
+const App: React.FC = () => {
+  const [name, setName] = React.useState('')
+  const [surname, setSurname] = React.useState('')
+  const [isActive, setIsActive] = React.useState(true)
+
+  const [fullNames, setFullName] = React.useState<
+    {
+      name: string
+      surname: string
+    }[]
+  >([{ name: '', surname: '' }])
+
+  useEffect(() => {
+    if (fullNames.length > 5) {
+      setIsActive(false)
+    }
+  }, [fullNames])
+
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setName(e.target.value)
+  }
+
+  const onChangeSurname = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setSurname(e.target.value)
+  }
+
+  const removeTodo = (index: number) => {
+    const newNames = [...fullNames]
+    newNames.splice(index, 1)
+    setFullName(newNames)
+  }
+
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault()
+    setFullName((names) => [...names, { name: name, surname: surname }])
+
+    setName('')
+    setSurname('')
+  }
+
+  const namesElements = fullNames.map((item, key) => {
+    if (!item.name || !item.surname) return <></>
+    return (
+      <div key={key}>
+        <div>
+          {item.name} {item.surname}
+        </div>
+        <button onClick={() => removeTodo(key)}>usuń</button>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-wrapper">
+        <div className="header" />
+      </div>
+
+      <div className="element-b">
+        <div className="element-c">
+          <div className="wrapper-e">
+            <div className="element-e">E</div>
+          </div>
+        </div>
+        <div className="table-wrapper">
+          <form className="message-form" onSubmit={onSubmit}>
+            Name:
+            <input
+              required
+              className="name-input"
+              onChange={onChangeName}
+              value={name}
+            />
+            Surname:
+            <input
+              required
+              className="surname-input"
+              onChange={onChangeSurname}
+              value={surname}
+            />
+            <button disabled={!isActive} type="submit">
+              SEND
+            </button>
+          </form>
+
+          {namesElements && (
+            <div>
+              <div className="box1">Pracownicy</div>
+              {namesElements}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="footer-wrapper">
+        <div className="footer" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
